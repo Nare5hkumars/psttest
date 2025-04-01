092b0425d40952ee579508e5457938dabf6442aa
Nare5hkumars <nareshkumarnkrs@gmail.com>
2025-04-01 20:02:48 +0530

    Update database.py

--- bot/database.py
+++ bot/database.py
@@ -1,1 +1,14 @@
+from pymongo import MongoClient
+from config import MONGO_URI
 
+mongo_cli=MongoClient(MONGO_URI)
+db=mongo_cli["media_bot"]
+posts_collection=db["posts"]
+
+def save_post(user_id,text=None,file_id=None):
+  post_data={"user_id":user_id,"text":text,"file_id":file_id}
+  posts_collection.insert_one(post_data)
+
+def get_last_post(user_id):
+  return
+posts_collection.find_one({"user_id":user_id},sort=[("_id",-1)])
