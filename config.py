f4e926c6de22bf2267b685ce1592a8db5f1ade9f
Nare5hkumars <nareshkumarnkrs@gmail.com>
2025-04-01 19:54:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API_ID=""
-API_HASH=""
-BOT_TOKEN=""
-MONGO_URI=""
+API_ID="27951862"
+API_HASH="efc22a9727c26a6f88f4f080bb284ac9"
+BOT_TOKEN="7283113363:AAHKo1DHrqlcnow9X9Ith1xlyOKsn4HOkTM"
+MONGO_URI="mongodb+srv://wixeti5091:KU5TTKZoMwEccqaK@cluster0.k0anxi4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
